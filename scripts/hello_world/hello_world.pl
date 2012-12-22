93ac71219eea0670ab2b2250ceba5d6dec9ab5ff
Steve Baker <ssbaker@liquidweb.com>
2012-12-22 12:49:13 -0700

    Create variable for pin

--- scripts/hello_world/hello_world.pl
+++ scripts/hello_world/hello_world.pl
@@ -7,18 +7,18 @@ use warnings;
 #Device::BCM2835::set_debug(1);
 Device::BCM2835::init() || die "Could not init library";
 
-# Blink pin 23:
-# Set RPi pin 23 to be an output
-Device::BCM2835::gpio_fsel(
-	&Device::BCM2835::RPI_GPIO_P1_23,
-	&Device::BCM2835::BCM2835_GPIO_FSEL_OUTP);
+# Blink pin:
+# Set RPi pin to be an output
+my $pin = Device::BCM2835::RPI_GPIO_P1_10; 
+Device::BCM2835::gpio_fsel($pin, Device::BCM2835::BCM2835_GPIO_FSEL_OUTP);
 
 while (1) {
 	# Turn it on
-	Device::BCM2835::gpio_write(&Device::BCM2835::RPI_GPIO_P1_23, 1);
+	Device::BCM2835::gpio_write($pin, 1);
 	Device::BCM2835::delay(500); # Milliseconds
-	                             # Turn it off
-	Device::BCM2835::gpio_write(&Device::BCM2835::RPI_GPIO_P1_23, 0);
+
+	# Turn it off
+	Device::BCM2835::gpio_write($pin, 0);
 	Device::BCM2835::delay(500); # Milliseconds
 }
 
