20f4d39ec74029236ffecc331cd65839927903bb
Steve Baker <ssbaker@liquidweb.com>
2013-01-06 08:35:44 -0700

    Comments showing possible setup

--- modules/RPi/MCP3008.pm
+++ modules/RPi/MCP3008.pm
@@ -8,6 +8,11 @@ sub new {
 	my $class = shift;
 
 	Device::BCM2835::spi_begin();
+#	Device::BCM2835::spi_setBitOrder(Device::BCM2835::BCM2835_SPI_BIT_ORDER_MSBFIRST);
+#	Device::BCM2835::spi_setDataMode(Device::BCM2835::BCM2835_SPI_MODE0);
+#	Device::BCM2835::spi_setClockDivider(Device::BCM2835::BCM2835_SPI_CLOCK_DIVIDER_65536);
+#	Device::BCM2835::spi_chipSelect(Device::BCM2835::BCM2835_SPI_CS0);
+#	Device::BCM2835::spi_setChipSelectPolarity(Device::BCM2835::BCM2835_SPI_CS0, 0);
 
 	my $self = {
 		commands => [qw/018000 019000 01a000 01b000 01c000 01d000 01e000 01f000/],
