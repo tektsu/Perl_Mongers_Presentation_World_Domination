e6a17f5e19e736bdfdc902cb3c067ae9af089f0d
Steve Baker <ssbaker@liquidweb.com>
2013-01-06 09:57:02 -0700

    Make use of changes in PWM module

--- scripts/expr/pwm.pl
+++ scripts/expr/pwm.pl
@@ -5,10 +5,7 @@ use lib qw|../../modules|;
 use RPi::PWM;
 use Time::HiRes qw/usleep/;
 
-my $pwm = RPi::PWM->new;
-$pwm->setDutyCycle(0);
-$pwm->setFrequency(100);
-$pwm->activate;
+my $pwm = RPi::PWM->new(dutyCycle => 0, frequency => 100, activate => 1);
 for (1 .. 3) {
 	for my $dutyCycle (0 .. 99) {
 		$pwm->setDutyCycle($dutyCycle);
@@ -19,7 +16,5 @@ for (1 .. 3) {
 		usleep(5000);
 	}
 }
-$pwm->deactivate;
-$pwm->setDutyCycle(0);
 
 
