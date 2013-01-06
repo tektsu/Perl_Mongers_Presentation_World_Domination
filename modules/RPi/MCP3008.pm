a34c03a294a5770ee3dd2cd70dff351307d2b084
Steve Baker <ssbaker@liquidweb.com>
2013-01-06 08:31:05 -0700

    Support for reading any channel. Release pins in destructor

--- modules/RPi/MCP3008.pm
+++ modules/RPi/MCP3008.pm
@@ -9,7 +9,9 @@ sub new {
 
 	Device::BCM2835::spi_begin();
 
-	my $self = {};
+	my $self = {
+		commands => [qw/018000 019000 01a000 01b000 01c000 01d000 01e000 01f000/],
+	};
 	bless $self, $class;
 
 	return $self;
@@ -17,10 +19,19 @@ sub new {
 
 sub readChannel {
 	my $self = shift;
+	my $channel = shift;
+	die "Channel [$channel} is invalid\n" if $channel < 0 || $channel > 7;
 
-	my $buffer = pack('H*', '018000');
+	my $buffer = pack('H*', $self->{commands}[$channel]);
 	Device::BCM2835::spi_transfern($buffer);
 	return hex(unpack('H*', $buffer)) & 0x3ff;
 }
 
+sub DESTROY {
+	my $self = shift;
+
+	Device::BCM2835::spi_end();
+}
+
+1;
 
