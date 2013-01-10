4f0253acc96b02b1e1e75477a3449f9fcae38095
Steve Baker <ssbaker@liquidweb.com>
2013-01-09 17:47:44 -0700

    Move PWM methods to a subclass

--- modules/RPi/PWM.pm
+++ modules/RPi/PWM.pm
@@ -3,19 +3,16 @@ use warnings;
 
 package RPi::PWM;
 
-my $controlDir = '/sys/class/rpi-pwm/pwm0';
-
 sub new {
 	my $class = shift;
 	my %args  = @_;
 
-	my $self = {};
+	my $self = {
+		controlDir => '/sys/class/rpi-pwm/pwm0',
+	};
 	bless $self, $class;
 
 	$self->deactivate;
-	$self->setDutyCycle($args{dutyCycle}) if exists $args{dutyCycle};
-	$self->setFrequency($args{frequency}) if exists $args{frequency};
-	$self->activate if $args{activate};
 
 	return $self;
 }
@@ -25,41 +22,15 @@ sub activate {
 }
 
 sub deactivate {
-	my $self = shift;
-	$self->_write('active', 0);
-}
-
-sub setDutyCycle {
-	my $self = shift;
-	my ($value) = @_;
-
-	$value ||= 1;;
-	$value = 99 if $value > 99;
-	$self->_write('duty', $value);
-}
-
-sub getDutyCycle {
-	my $self = shift;
-	my ($value) = @_;
-
-	return $self->_read('duty');
-}
-
-sub setFrequency {
-	my $self = shift;
-	my ($value) = @_;
-
-	$value = 0 if $value < 0;
-	$value = 16000 if $value > 16000;
-	$self->_write('frequency', $value);
+	shift->_write('active', 0);
 }
 
 sub _read {
 	my $self = shift;
 	my ($file) = @_;
 
-	open my $fh, '<', "$controlDir/$file"
-		or die "Unable to open $controlDir/$file: $!\n";
+	open my $fh, '<', "$self->{controlDir}/$file"
+		or die "Unable to open $self->{controlDir}/$file: $!\n";
 	my $value = <$fh>;
 	close $fh;
 
@@ -70,17 +41,14 @@ sub _write {
 	my $self = shift;
 	my ($file, $value) = @_;
 
-	open my $fh, '>', "$controlDir/$file"
-		or die "Unable to open $controlDir/$file: $!\n";
+	open my $fh, '>', "$self->{controlDir}/$file"
+		or die "Unable to open $self->{controlDir}/$file: $!\n";
 	print $fh $value;
 	close $fh;
 }
 
 sub DESTROY {
-	my $self = shift;
-
-	$self->deactivate;
-	$self->setDutyCycle(0);
+	shift->deactivate;
 }
 
 1;
--- scripts/expr/pwm.pl
+++ scripts/expr/pwm.pl
@@ -2,13 +2,13 @@
 use strict;
 use warnings;
 use lib qw|../../modules|;
-use RPi::PWM;
+use RPi::PWM::PWM;
 use Time::HiRes qw/usleep/;
 
 exec "sudo $0 " . join(' ', @ARGV) if $<;
 $SIG{INT} = sub { exit; };
 
-my $pwm = RPi::PWM->new(dutyCycle => 0, frequency => 100, activate => 1);
+my $pwm = RPi::PWM::PWM->new(dutyCycle => 0, frequency => 100, activate => 1);
 for (1 .. 3) {
 	for my $dutyCycle (0 .. 99) {
 		$pwm->setDutyCycle($dutyCycle);
